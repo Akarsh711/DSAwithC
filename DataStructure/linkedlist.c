310cf04ca04fe8d6a779452800d937f623906bf1
Akarsh369 <48852000+Akarsh711@users.noreply.github.com>
2021-02-09 18:14:39 +0530

    Add files via upload

--- DataStructure/linkedlist.c
+++ DataStructure/linkedlist.c
@@ -14,6 +14,13 @@ struct Node * createNode(int data){
     return n;
 }
 
+struct Node * prepend(struct Node * start_node, int data){
+    /* TODO: Add a Check if list have only one node or none*/
+    struct Node * n = createNode(data);
+    n->next = start_node;
+    return n;
+}
+
 // LOOPY FUNCITONS
 void traverseLoop(struct Node *start_node){
     while(start_node){
@@ -41,15 +48,24 @@ void searchLoop(struct Node * start_node, int to_search){
     }
 }
 
-struct Node * prependLoop(struct Node * start_node, int data){
-    /* TODO: Add a Check if list have only one node or none*/
-    struct Node * n = createNode(data);
-    n->next = start_node;
-    return n;
+void insertAfterNodeLoop(struct Node *start_node, int data, int insert_after){
+    struct Node *n = createNode(data);
+    while(start_node){
+        if(start_node->data == insert_after){
+            n->next = start_node->next;
+            start_node->next = n;
+            printf("\nin fi");
+            return; // You can also put this inefficient approach [start_node = start_node->next];
+            
+        }
+        else{
+            start_node = start_node->next;
+        }
+    }
 }
 
 
-// RECURSIVE FUNCTIONS
+// RECURSIVE FUNCTIONS -----------------
 void traverse(struct Node *start_node){
     if(start_node){ //Putting start_node->data give me segementation fault!! [RESEARCHABLE THING!]
         printf("%d",start_node->data);
@@ -75,9 +91,17 @@ void search(struct Node* start_node, int to_search){
     }
 }
 
-// struct Node * insert(struct Node *start_node, int data){
-//     struct Node * n = createNode(data);
-// }
+void insertAfterNode(struct Node *start_node, int data, int after_which_node){
+    /* TODO: Add Exception Handler or if else for [if node is empty or single or not present]*/
+    struct Node * n = createNode(data);
+    if(start_node->data == after_which_node){
+        n->next = start_node->next;
+        start_node->next = n;
+    }
+    else{
+        insertAfterNode(start_node->next, data, after_which_node);
+    }
+}
 
 
 int main(){
@@ -85,12 +109,12 @@ int main(){
     start_node->next = createNode(6);
     start_node->next->next = createNode(9);
     insert(start_node, 10);
-    start_node = prependLoop(start_node, 1);
+    start_node = prepend(start_node, 1);
     insertLoop(start_node, 11);
     searchLoop(start_node, 11);
     search(start_node, 9);
+    insertAfterNodeLoop(start_node, 66, 11);
     traverse(start_node);
     traverseLoop(start_node);
-
     printf("\ntest");
 }
