1ef111e513f8797d81d2bd4180226c2a7e9de0b9
Akarsh <paliwalap7@gmail.com>
2021-04-19 01:57:23 +0530

    added bbst from sorted array

---
+++ DataStructure/Trees/balanced_bst_from_sorted_arr.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+	int data;
+	struct Node *left;
+	struct Node *right;
+
+	Node(int val){
+		data = val;
+		left = NULL;
+		right = NULL;
+		}
+};
+
+
+struct Node *buildTree(int arr[], int l, int r){
+	if(l == r){
+		return new Node(arr[l]);
+	}
+	else if(l>r){
+		return NULL;
+	}
+
+	int mid = (l+r)/2;
+	struct Node *root = new Node(arr[mid]);
+	
+	root->left = buildTree(arr, l, mid-1);
+	root->right = buildTree(arr, mid+1, r);
+	return root;
+}
+
+void inOrder(struct Node *root){
+	if(root == NULL){
+		return;
+	}
+	inOrder(root->left);
+	cout <<root->data<< " ";
+	inOrder(root->right);
+}
+
+int main(){
+	int arr[] = {1,2,4,5,6};
+
+	struct Node *root = new Node(2);
+	// root->left = new Node(1);
+	// root->right = new Node(3);
+	root = buildTree(arr, 0, 4);
+	inOrder(root);
+}
