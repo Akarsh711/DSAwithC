6cc03075a6e554e956547a1aa5e9f95e8ccbd394
Akarsh <paliwalap7@gmail.com>
2021-09-21 00:15:00 +0530

    added POINTMEE documentation is left

--- sol/POINTMEE.cpp
+++ sol/POINTMEE.cpp
@@ -1,37 +1,76 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+// typedef map<pair<int, int>, vector<vector<int>>> tdvecmap;
 
-struct Node{
+auto moves(tuple<int, int, int> point, int n, vector<pair<int, int>> p)
+{
+    auto [x, y, den] = point;
+    int cur = 2 * n;
+    if (x == INT_MAX)
+        return cur;
+    for (int i = 0; i < n; i++)
+    {
+        auto [xi, yi] = p[i];
+        int ax = xi * den, ay = yi * den;
+        if (ax == x && ay == y)
+            cur -= 2;
+        else if (ax == x || ay == y || (ax - ay) == (x - y) || (ax + ay) == (x + y))
+            cur--;
+    }
+    return cur;
+}
 
-};
+auto intersect(auto line1, auto line2){
+    int a1= line1[0];
+    int b1 = line1[1];
+    int c1 = line1[2];
+    int a2 = line2[0];
+    int b2 = line2[1];
+    int c2 = line2[2];
 
-int main(){
-    int t;
-    cin>>t;
+    // auto [a2 ,b2, c2] = line2;
+    int den =a1*b2 - a2*b1;
+    int x = 0;
+    int y = 0;
+    // cout<<den<<endl;
+    if(den!=0){
+        x = b1 * c2 - b2 * c1;
+        y = a2 * c1 - a1 * c2;
+    }
 
+// //Not know why
+// if (den < 0) {
+//             den = -den;
+//             x = -x;
+//             y = -y;
+//         }
 
-    while(t--){
+// // cout<<"working"<<endl;
+return make_tuple(x, y, den);
+// }
+
+int main()
+{
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
         int N;
-        cin>>N;
+        cin >> N;
 
-        vector<int> x;
-        vector<int> y;
+        vector<pair<int, int>> p(N); //points
         map<pair<int, int>, vector<vector<int>>> eqs;
-        vector<int> ip; //intersecting points;
+        vector<pair<int, int>> ip; //intersecting points;
 
-        for(int i=0;i<N; i++){
-            int temp;
-            cin>>temp;
-            x.push_back(temp);
-        }
-        for(int i=0; i<N; i++){
-            int temp;
-            cin>>temp;
-            y.push_back(temp);
-        }
+        for (int i = 0; i < N; i++)
+            cin >> p[i].first;
+        for (int i = 0; i < N; i++)
+            cin >> p[i].second;
 
         // Line Equation
-        for(int i=0; i<N; i++){
+        for (int i = 0; i < N; i++)
+        {
             /*
                 1, 0, -xi ++
                 0, 1, -yi ++
@@ -48,30 +87,65 @@ int main(){
                 135 deg         1         1
                 180 deg         1         0
             */
-            
-            eqs[{x[i], y[i]}][0] = {0, 1, -x[i]};
-            eqs[{x[i], y[i]}][1] = {1, 0, -y[i]};
-            eqs[{x[i], y[i]}][2] = {1,1 ,(y[i]+x[i])};
-            eqs[{x[i], y[i]}][3] = {1, -1, (y[i]-x[i])};
+
+            eqs[{p[i].first, p[i].second}].push_back({1,0,-p[i].first});
+            eqs[{p[i].first, p[i].second}].push_back({0,1, -p[i].second});
+            eqs[{p[i].first, p[i].second}].push_back({1, -1, (-p[i].first + p[i].second)});
+            eqs[{p[i].first, p[i].second}].push_back({1, 1, (-p[i].first - p[i].second)});
         }
 
         // Finding intersection points
-        // void intersect(auto line1, auto line2){
-            
-        // }
+        // auto intersect = [&](auto line1, auto line2)
+        // {
+        //     int a1 = line1[0];
+        //     int b1 = line1[1];
+        //     int c1 = line1[2];
+        //     int a2 = line2[0];
+        //     int b2 = line2[1];
+        //     int c2 = line2[2];
 
-        
-        for(int i=0; i<N; i++){
-            for(int j=i+1; j<N; j++){
-                //loop for line for point A.
-                for(int k=0; k<4; k++){
-                    // loop for line for point B.
-                    for(int l=0; l<4; l++){
+        //     int den = a1 * b2 - b1 * a2, x = 2e18, y = 0;
+        //     if (den != 0)
+        //     {
+        //         x = b1 * c2 - b2 * c1;
+        //         y = a2 * c1 - a1 * c2;
+        //     }
+        //     if (den < 0)
+        //     {
+        //         den = -den;
+        //         x = -x;
+        //         y = -y;
+        //     }
+        //     return make_tuple(x, y, den);
+        // };
 
+        int ans = 2 * N;
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = i + 1; j < N; j++)
+            {
+                //loop for lines for point A.
+                for (int k = 0; k < 4; k++)
+                {
+                    // loop for lines for point B.
+                    for (int l = 0; l < 4; l++)
+                    {
+                        pair<int, int> pointa = {p[i].first, p[i].second};
+                        pair<int, int> pointb = {p[j].first, p[j].second};
+
+                        vector<int> line1 = eqs[pointa][k];
+                        vector<int> line2 = eqs[pointb][l];
+
+                        auto point = intersect(line1, line2);
+                        ans = min(ans, moves(point, N, p));
                     }
                 }
             }
         }
-        
+
+        cout << ans << endl;
+        // void min()
     }
+    return 0;
 }
--- sol/test.cpp
+++ sol/test.cpp
@@ -1,15 +1,155 @@
 #include<bits/stdc++.h>
+#define ll long long
+#define int long long
 using namespace std;
 
+// auto intersect(auto line1, auto line2){
+//     // int a1= line1[0];
+//     // int b1 = line1[1];
+//     // int c1 = line1[2];
+//     // int a2 = line2[0];
+//     // int b2 = line2[1];
+//     // int c2 = line2[2];
+    
+//     auto [a1, b1, c1] = line1;
+//     auto [a2 ,b2, c2] = line2;
+//     int den =a1*b2 - a2*b1;
+//     int x = 0; 
+//     int y = 0;
+//     // cout<<den<<endl;
+//     if(den!=0){
+//     x = (c1*b2 - c2*b1)/den;
+//     y = (a1*c2 - a2*c1)/den;
+//     }
+ 
+//     // cout<<"working"<<endl;
+//     return make_tuple(x, y, den);
+//     }
 
-void test(auto eq){
-  eq[{1,2}][0] = {0,1};
-  cout<<eq[{1,2}];
-}
-int main(int argc, char const *argv[])
+
+int32_t main()
 {
-  map<pair<int, int>, vector<vector<int>>> eqs;
-  cout<<"running...";
-  test(eqs);
-  return 0;
+    ll TC;
+    cin >> TC;
+    while (TC--)
+    {
+        // ll n; cin >> n;
+        // vector<pair<int, int>> pt(n);
+        // map<tuple<int, int, int>, int> m[4];
+        // for (int i = 0; i < n; i++)cin >> pt[i].first;
+        // for (int i = 0; i < n; i++)cin >> pt[i].second;
+        // for (int i = 0; i < n; i++) {
+        //     auto [xi, yi] = pt[i];
+        //     m[0][ {1, 0, -xi}]++; // stores vertical lines
+        //     m[1][ {0, 1, -yi}]++; // stores horizontal lines
+        //     m[2][ {1, -1, -xi + yi}]++; // stores / lines
+        //     m[3][ {1, 1, -xi - yi}]++; // stores \ lines
+        // }
+
+int N;
+        cin >> N;
+
+        vector<pair<int, int>> p(N); //points
+        map<pair<int, int>, vector<vector<int>>> eqs;
+        vector<pair<int, int>> ip; //intersecting points;
+
+        for (int i = 0; i < N; i++)
+            cin >> p[i].first;
+        for (int i = 0; i < N; i++)
+            cin >> p[i].second;
+
+        // Line Equation
+        for (int i = 0; i < N; i++)
+        {
+            /*
+                1, 0, -xi ++
+                0, 1, -yi ++
+                1, -1, -xi+yi ++
+                1, 1 , -xi-yi ++
+            */
+
+            // [a, b, c]4
+
+            /*
+                Type of lines    x      y
+                -----------------------------
+                90 deg          0         1
+                135 deg         1         1
+                180 deg         1         0
+            */
+
+            eqs[{p[i].first, p[i].second}].push_back({0, 1, -p[i].first});
+            eqs[{p[i].first, p[i].second}].push_back({1, 0, -p[i].second});
+            eqs[{p[i].first, p[i].second}].push_back({1, 1, (p[i].first + p[i].second)});
+            eqs[{p[i].first, p[i].second}].push_back({1, -1, (p[i].first - p[i].second)});
+        }
+        auto intersect = [&](auto line1 , auto line2) {
+            auto [a1, b1, c1] = line1;
+            auto [a2, b2, c2] = line2;
+            int den = a1 * b2 - b1 * a2, x = 2e18, y = 0;
+            if (den != 0) {
+                x = b1 * c2 - b2 * c1; y = a2 * c1 - a1 * c2;
+            }
+            if (den < 0) {
+                den = -den;
+                x = -x;
+                y = -y;
+            }
+            return make_tuple(x, y, den);
+        };
+
+        auto moves = [&](tuple<ll, ll, ll> point) {
+            auto [x, y, den] = point;
+            int cur = 2 * n;
+            if (x == 2e18) return cur;
+            for (int i = 0; i < n; i++) {
+                auto [xi, yi] = pt[i];
+                int ax = xi * den, ay = yi * den;
+                if (ax == x && ay == y) cur -= 2;
+                else if (ax == x || ay == y || (ax - ay) == (x - y) || (ax + ay) == (x + y)) cur--;
+            }
+            return cur;
+        };
+
+        int ans = 2 * n;
+        //If final intersection point is one of the initial points
+        for (int i = 0; i < n; i++) {
+            auto [x, y] = pt[i];
+            ans = min(ans, moves({x , y, 1}));
+        }
+
+        /*If final point is intersection of given lines (considering all pair of lines with different slopes)
+        NOTE - Lines with same slopes never intersect */
+        // for (int i = 0; i < 4; i++) {
+        //     for (int j = i + 1; j < 4; j++) {
+        //         for (auto [line1 , cnt1] : m[i]) {
+        //             for (auto [line2 , cnt2] : m[j]){
+        //                 auto point = intersect(line1, line2);
+        //                 ans = min(ans , moves(point));
+        //             }
+        //         }
+        //     }
+        // }
+
+
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = i + 1; j < N; j++)
+            {
+                //loop for lines for point A.
+                for (int k = 0; k < 4; k++)
+                {
+                    // loop for lines for point B.
+                    for (int l = 0; l < 4; l++)
+                    {
+                        auto point = intersect(line1, line2);
+                        ans = min(ans , moves(point));
+                    }
+                }
+            }
+        }
+        cout << ans << '\n';
+    }
+
+    return 0;
 }
