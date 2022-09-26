624ba53f223dead6c06171338fc594a4fdfb52a5
Akarsh711 <paliwalap7@gmail.com>
2022-09-26 17:14:08 +0530

    added program for size of Generic Tree and Max of Generic Tree

---
+++ Generic Tree/MaxofGenericTree.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+    int data;
+    vector<Node *> children;
+};
+
+Node* newNode(int data){
+    Node * node = new Node();
+    node->data = data;
+    return node;
+}
+
+Node *constructor(vector<int> arr, int n){
+    Node * root = NULL;
+    stack<Node*> st;
+    for(int i=0; i<n; i++){
+        if(arr[i] == -1){
+            st.pop();
+        }else{
+            Node * t = newNode(arr[i]);
+            if(st.size() == 0){
+                root = t;
+                st.push(t);
+            }
+            else{
+                st.top()->children.push_back(t);
+            }
+            st.push(t);
+        }
+    }
+    return root;
+}
+
+void Display(Node * root){
+    if(root == NULL) return;
+    
+    for(auto i: root->children){
+        cout<<i->data<<" ";
+        Display(i);
+    }
+}
+
+int findMax(Node * root){
+    if(root == NULL) return 0;
+    int mx = 0;
+    mx = root->data;
+    for(auto i: root->children){
+        mx = max(mx, findMax(i));
+    }
+    return mx;
+}
+
+int main()
+{
+    // Generic Tree Constructor
+    vector<int> arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100};
+    Node * root = constructor(arr, 21);
+    Display(root);
+    cout<<"\n"<<findMax(root);
+    return 0;
+}
