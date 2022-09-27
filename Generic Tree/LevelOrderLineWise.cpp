e9aca04fcdf4ea9ba71381bd195e76f879724ad9
Akarsh711 <paliwalap7@gmail.com>
2022-09-28 04:39:51 +0530

    added level order traversal line wise of generic tree

---
+++ Generic Tree/LevelOrderLineWise.cpp
@@ -0,0 +1,78 @@
+ #include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node{
+    int data;
+    vector<Node *> children;
+};
+
+Node* newNode(int data){
+    Node * node = new Node();
+    node->data = data;
+    return node;
+}
+
+Node *constructor(vector<int> arr, int n){
+    Node * root = NULL;
+    stack<Node*> st;
+    for(int i=0; i<n; i++){
+        if(arr[i] == -1){
+            st.pop();
+        }else{
+            Node * t = newNode(arr[i]);
+            if(st.size() == 0){
+                root = t;
+                st.push(t);
+            }
+            else{
+                st.top()->children.push_back(t);
+            }
+            st.push(t);
+        }
+    }
+    return root;
+}
+
+void Display(Node * root){
+    if(root == NULL) return;
+    
+    for(auto i: root->children){
+        cout<<i->data<<" ";
+        Display(i);
+    }
+}
+
+void levelOrder(Node * root){
+    if(!root) return;
+    queue<Node *> q;
+    q.push(root);
+    while(q.size() > 0){
+        Node * temp = q.front();
+        if(temp == NULL){
+            cout<<"\n";
+            continue;
+        }
+        cout<<temp->data<<" ";
+        
+        
+        for(auto i: temp->children){
+            q.push(i);
+        }
+        q.push(NULL);
+        q.pop();
+    }
+
+    
+}
+int main()
+{
+    // Generic Tree Constructor
+    vector<int> arr = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100};
+    Node * root = constructor(arr, 21);
+    Display(root);
+    cout<<"\n";
+    levelOrder(root);
+    
+    return 0;
+}
