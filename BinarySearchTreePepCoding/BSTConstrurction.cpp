91ea48d63b2c9b581de8456b8852e1cf1bd7568e
Akarsh711 <paliwalap7@gmail.com>
2022-11-20 11:00:57 +0530

    BSTConstruction.cpp

---
+++ BinarySearchTreePepCoding/BSTConstrurction.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node * left = NULL;
+    Node * right = NULL;
+    Node(int val, Node *lc, Node *rc){
+        left = lc;
+        right = rc;
+        data = val;
+    }
+};
+
+struct Node * constructBST(int arr[], int lb, int ub){
+    if(lb>ub)return NULL;
+    int mid = lb+ub/2;
+    Node * lc = constructBST(arr, lb, mid-1);
+    Node * rc = constructBST(arr, mid+1 ,ub);
+
+    Node * root = new Node(arr[mid], lc, rc);
+    return root;
+    
+}
+
+void display(Node * root){
+    if(root == NULL)return;
+    cout<<root->data;
+    display(root->left);
+    display(root->right);
+}
+
+int main(){
+    int arr[] = {12,25, 37, 50, 62, 75, 87};
+    int size = sizeof(arr)/sizeof(int);
+    Node * root = constructBST(arr, 0, size-1);
+    display(root);
+    return 0;
+}
