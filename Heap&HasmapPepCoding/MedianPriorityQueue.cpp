85a24c698b3060a277eae5108feb934d09a83324
Akarsh711 <paliwalap7@gmail.com>
2022-12-08 06:17:03 +0530

    --

---
+++ Heap&HasmapPepCoding/MedianPriorityQueue.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MedianPriortiyQueue{
+    public:
+        priority_queue<int> left;
+        priority_queue<int, vector<int>, greater<int>> right;
+
+        int sizeofmq(){
+            return left.size() + right.size();
+        }
+
+        void add(int val){
+            if(right.size() > 0 && val> right.top()){
+                right.push(val);
+            } else{
+                left.push(val);
+            }
+
+            if(left.size() - right.size() == 2){
+                right.push(left.top());
+                left.pop();
+            } else if(right.size() - left.size() == 2){
+                left.push(right.top());
+                right.pop();
+            }
+        }
+    
+        int peek(){
+            if(sizeofmq() == 0){
+                cout<<"Underflow";
+                return -1;
+            }
+
+            if(left.size()>=right.size()){
+                return left.top();
+            } else{
+                return right.top();
+            }
+        }
+
+        void remove(){
+            if(sizeofmq() == 0){
+                cout<<"Underflow";
+                return;
+            }
+
+            if(left.size()>=right.size()){
+                return left.pop();
+            } else{
+                return right.pop();
+            }
+        }
+        
+};
+
+int main(){
+    // MedianPriortiyQueue obj;
+    // obj.add(1);
+    // obj.add(2);
+    // obj.add(3);
+    // cout<<obj.peek();
+    int t;
+    while(t--){
+        MedianPriortiyQueue obj;
+        int n;
+        cin>>n;
+        for(int i=0; i<n; i++){
+            int x;
+            cin>>x;
+            obj.add(x);
+            cout<<floor(obj.peek())<<"\n";
+        }
+    }
+    return 0;
+}
